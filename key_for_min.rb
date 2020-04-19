3b80d2290204fa828960abc1d39685a6429a7227
Erin Mettille <ecmettille@gmail.com>
2020-04-19 01:28:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,14 +11,16 @@ def key_for_min_value(name_hash)
   
   name_hash.each do |key, value|
     value_array << value
-  
+    
     if value <= value_array[0] = TRUE
       return key
     end
   end
   
-  
 end
+
+
+
 
 counter = 0
  
