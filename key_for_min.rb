821f723601f8c716e5cda283eadf5e3dddf75632
Erin Mettille <ecmettille@gmail.com>
2020-04-19 01:58:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,15 +10,12 @@ def key_for_min_value(name_hash)
   
   min_val = 0 # def min value 
  
-# loop through each item in ints list
   for item in values:
-	# if the current item is less than the item before it and it is less than our current min_val then we assign min_val to that item
-	if item < ints[ints.index(item)-1] and item < min_val:
-		min_val = item
-# print the min_val
-print(min_val)
+  	if item < values[values.index(item)-1] && item < min_val
+		  min_val = item
+    end
 
-  name_hash.each do |key, value|
+  
   
   end
   
