fefe3957894b4f37b8eaa586ad51ba08169b221d
Erin Mettille <ecmettille@gmail.com>
2020-04-19 01:42:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,14 +5,8 @@
 # Need to return key
 
 def key_for_min_value(name_hash)
-  return [] if name_hash.empty?
-  value_array = []
   
   name_hash.each do |key, value|
-    value_array << value
-    if value <= value_array == TRUE
-      return key
-    end
   
   end
   
